eeba813401e0d06808b25ba62a118e96e99b36bb
basel-abdelazim <basel.abdelazim@gmail.com>
2024-08-26 13:57:37 +0200

    Update MovieGrid.jsx

--- Desktop/cineverse2/src/components/MovieGrid.jsx
+++ Desktop/cineverse2/src/components/MovieGrid.jsx
@@ -12,7 +12,7 @@ const MovieGrid = () => {
 
   const fetchMovies = async () => {
     try {
-      const response = await fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', {
+      const response = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', {
         method: 'GET',
         headers: {
           accept: 'application/json',
