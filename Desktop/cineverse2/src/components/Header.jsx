f2884547009ea59a4db54f7c7e21b92a5d113f1e
basel-abdelazim <basel.abdelazim@gmail.com>
2024-08-29 13:48:04 +0200

    Update Header.jsx

--- Desktop/cineverse2/src/components/Header.jsx
+++ Desktop/cineverse2/src/components/Header.jsx
@@ -1,35 +1,26 @@
-import React, { useState } from "react";
-import { translations } from "./translations"; // Adjust the import path as necessary
+import PropTypes from 'prop-types';
 
-const Header = () => {
-  const [language, setLanguage] = useState("en"); // Default language is English
-
-  const handleTranslate = (lang) => {
-    setLanguage(lang); // Set the current language
-  };
-
+const Header = ({ title, searchTerm, onSearchChange }) => {
   return (
-    <header className="bg-gray-900 text-white p-4">
-      <div className="container mx-auto flex justify-between">
-        <div className="flex space-x-4">
-          <a href="/" className="hover:text-gray-400">
-            {translations[language].home}
-          </a>
-          <a href="/journal" className="hover:text-gray-400">
-            {translations[language].journal}
-          </a>
-        </div>
-        <div>
-          <button onClick={() => handleTranslate("de")} className="text-white">
-            Deutsch
-          </button>
-          <button onClick={() => handleTranslate("en")} className="text-white">
-            English
-          </button>
-        </div>
+    <header className="p-4 bg-gray-800 flex justify-between items-center">
+      <h1 className="text-white text-xl">{title}</h1>
+      <div className="search-bar relative w-1/2">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => onSearchChange(e.target.value)}
+          className="w-full px-5 py-2 rounded-lg bg-white text-black outline-none"
+        />
       </div>
     </header>
   );
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  searchTerm: PropTypes.string.isRequired,
+  onSearchChange: PropTypes.func.isRequired,
 };
 
 export default Header;
