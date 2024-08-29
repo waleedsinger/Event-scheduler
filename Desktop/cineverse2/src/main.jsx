1b5743d9318d7f918a4195ca6c204de9cccb1750
basel-abdelazim <basel.abdelazim@gmail.com>
2024-08-29 17:25:20 +0200

    Delete Desktop/cineverse2 directory

--- Desktop/cineverse2/src/main.jsx
+++
@@ -1,7 +0,0 @@
-// src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
